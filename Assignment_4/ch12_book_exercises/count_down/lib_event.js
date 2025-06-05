c75b271b3a08041ae3ba7b6f3b039bf5ff294585
THarris06 <144939694+THarris06@users.noreply.github.com>
2025-06-05 12:20:41 -0230

    giuyug

--- Assignment_4/ch12_book_exercises/count_down/lib_event.js
+++ Assignment_4/ch12_book_exercises/count_down/lib_event.js
@@ -16,4 +16,25 @@ class Event {
     get isValidDate() {
         return this.date.toString() != 'Invalid Date';
     }
+
+    get days() {
+        const today = new Date();
+        const msFromToday = this.date.getTime() - today.getTime();
+        const msForOneDay = 24 * 60 * 60 * 1000; // hrs * mins * secs * milliseconds  
+        return Math.ceil( msFromToday / msForOneDay ); 
+    }
+
+    get message() {
+        let msg = "";
+        const date = this.date.toDateString();
+        if (this.days == 0) {
+            msg = `Hooray! Today is ${this.name}! (${date})`;
+        } else if (this.days > 0) {
+            msg = `${this.days} day(s) until ${this.name}! (${date})`;
+        } else if (this.days < 0) {
+            msg = `${this.name} happened ${Math.abs(this.days)} 
+                   day(s) ago. (${date})`;
+        }
+        return msg;
+    }
 }
